c2076ff49da6aea6fc013466e6e0f41bd92c1d4c
Peter <pkkalinowski@oakland.edu>
2017-01-11 15:05:45 -0500

    index.php
    new index page

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/* Adds in the navigation bar the top of the page.*/
+/* include('head.php'); */
+include('header.php');
+?>
+
+<div class="start-screen">
+    <div class="container main-logo">
+        <div class="col-md-12 center">
+            <img src="img/trailmix-logo.png">
+        </div>
+    </div>
+    <div class="center">
+        <h2>Movement on your trail?</h2>
+        <h1>Instant notification.</h1>
+
+        <!-- <a href="#" class="btn btn-trailmix cta">Start Now</a> -->
+        
+    </div>
+</div>
+ 
+<?php
+/* Adds in the closing code, and footer information.*/
+include('foot.php');
+?>
