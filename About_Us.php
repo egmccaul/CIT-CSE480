fcc6a2c909bbba5d5ecbf8bd5f04ee5557933d7e
Peter <pkkalinowski@oakland.edu>
2017-01-11 15:10:00 -0500

    new about page
    article change to bacon ipsum for now

---
+++ About_Us.php
@@ -0,0 +1,28 @@
+<?php
+/* Adds in the navigation bar the top of the page.*/
+include('header.php');
+?>
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+        	<!--Add the header to the top of the jumbotron.-->
+        	<h1>About Us</h1>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="Photos/karatebelts.jpg"></img>
+            </div>
+            <div class="col-sm-8">
+                <!--Creates the paragraphs that will be held in the jumbotron.-->
+                <article id="about">
+                    <p>Burgdoggen corned beef tenderloin shankle cupim tongue, pig ribeye andouille beef ribs sausage biltong boudin. Alcatra bresaola pastrami andouille. Jerky jowl pork, ground round kielbasa pastrami t-bone sausage brisket swine drumstick shankle bacon tongue tri-tip. Jowl swine bacon pork chop kevin t-bone. Pig pork chop turducken, porchetta tri-tip tongue pork.</p>
+                </article>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+/* Adds in the closing code, and footer information.*/
+include('foot.php');
+?>
