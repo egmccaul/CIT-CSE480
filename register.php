0671b723ba910c10c4086a75c1aebf078019ca57
egmccaul <egmccaul@oakland.edu>
2017-01-23 15:55:57 -0500

    Add code to push registration to database

--- register.php
+++ register.php
@@ -1,6 +1,51 @@
 <?php
 /* Adds in the navigation bar the top of the page.*/
 include('header.php');
+
+// If form submitted, insert values into the database.
+	if (isset($_POST['email'])){
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		// PDO check for previous user
+			$statement = $dbh->prepare("SELECT * FROM USER WHERE USER_PASS = :pass AND USER_EMAIL = :email");
+			// PDO binds entry to protect against SQL Injection
+			$statement->bindParam(':pass', $password, PDO::PARAM_STR);
+			$statement->bindParam(':email', $email, PDO::PARAM_STR);
+
+			// Executes query
+			$executed = $statement->execute();
+
+			// Count number of rows
+			$number_rows = $statement->fetchColumn();
+
+			// If a user is found to match the criteria
+			if ($number_rows > 0) {
+				// Display a user found message.
+				?> <script>window.alert('Email Already Found!');</script> <?php
+			} else {
+
+				// PDO database entry query - (Removed state due to issues inserting into database, works after removal, defaulted to "Michigan")
+				$insertState = $dbh->prepare("INSERT INTO Student(USER_FNAME, USER_LNAME, USER_EMAIL, USER_PASS) VALUES (:fname, :lname, :email, :password)");
+				
+				// PDO binds entry to protect against SQL Injection
+				$insertState->bindParam(':fname', $fname, PDO::PARAM_STR);
+				$insertState->bindParam(':lname', $lname, PDO::PARAM_STR);
+				$insertState->bindParam(':email', $email, PDO::PARAM_STR);
+				$insertState->bindParam(':password', $password, PDO::PARAM_STR);
+
+				// Executes query
+				$insertExecuted = $insertState->execute();
+
+				if ($insertExecuted = true){
+					// Redirects after successful to home page
+					header("Location: index.php");
+				}
+			}
+
+	}else{
 ?>
 
 <div class="start-screen">
@@ -51,6 +96,9 @@ include('header.php');
 </div>
 
 <?php
+		/* Closes above PHP and MySQL Query. */
+	}
+		
 /* Adds in the footer at the bottom of the page.*/
 include('footer.php');
 ?>
