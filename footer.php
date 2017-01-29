054e0991577537892c44dd0dcd83871fc31aabb0
Patrick Cordero <pacordero@oakland.edu>
2017-01-29 13:40:57 -0500

    References to content and adding js files for code to work

--- footer.php
+++ footer.php
@@ -6,12 +6,15 @@
 				Tyler Miller</p>
             </footer>
         </div>
-    
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!-- End pagewrapper div removed from here-->
-</div>
+</div><!-------- END PAGE_WRAPPER -------->
+
+<script src="js/waypoints.min.js" type="text/javascript"></script>
+<script src="js/trailmix.js" type="text/javascript"></script>
 </body>
 </html>
