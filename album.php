e01648c33cac252b1bb3fd5588de85e02ea3d832
anaeem009 <anaeem@oakland.edu>
2017-02-07 23:38:50 -0500

    Album page basic
    A very basic submit of the album page. Still working on multiple rows and responsiveness. Will talk about the look with group.

---
+++ album.php
@@ -0,0 +1,105 @@
+
+<html>
+    <head>
+        <style type="text/css">
+     body{
+         margin-left: 0px;
+         margin-top: 0px;
+         margin-right: 0px;
+         margin-bottom: 0px;
+         background-color:#C0c0c0;
+     }
+     #container{
+         background-color:gray;
+         max-width:1500px;
+         height: auto;
+         margin-left: auto;
+         margin-right:auto;
+     }
+     #row1{
+         /*background-color:blue;*/
+         max-width:1300px;
+         margin-left: auto;
+         margin-right:auto;
+     }
+     #row2{
+         background-color:red;
+         max-width:1300px;
+         margin-left: auto;
+         margin-right:auto;
+         
+     }
+     #row3{
+         background-color:white;
+         max-width:1300px;
+         margin-left: auto;
+         margin-right:auto;
+         
+     }
+     .float{
+         display:inline-block;
+         height:200px;
+         width: 200px;
+         margin-left:auto;
+         margin-right:auto;
+          float:left;
+     }
+     #picture1{
+      width:400px;
+      height:268px;
+     padding-right: 50px;
+     padding-left:0px;
+     }
+       #picture2{
+      width:400px;
+      height:268px;
+     
+      padding-right: 50px;
+     padding-left:117px;
+     }
+     #picture3{
+       width:400px;
+       height:268px;
+        padding-right: 0px;
+     padding-left:50px;
+     }
+     #picture4{
+       
+     }
+     #picture5{
+       
+     }
+     
+        </style>
+    </head>
+    
+    <body>
+       <div id="container">
+         
+       <div id="row1">
+           <div class="float">
+               <img src="TestImages/featured.jpg"id="picture1" alt="picture1" />
+               </div>
+           <div class="float" id="picture2">
+               <img src="TestImages/07-trail-cam-animals-funny-when-humans-arent-around.jpg" id="picture2" alt="picture2" />
+               </div>
+           <div class="float" id="picture3">
+               <img src="TestImages/8_400w1.jpg" id="picture3" alt="picture3" />
+               </div>
+           </div>
+       <!--<div id="row2">-->
+       <!--    ROW 2-->
+       <!--    <div class="float" id="picture4">pictue4</div>-->
+       <!--    <div class="float" id="picture5">picture5</div>-->
+       <!--    <div class="float" id="picture6">picture6</div>-->
+           
+       <!--    </div>-->
+       <!-- <div id="row3">-->
+       <!--    ROW 3-->
+       <!--    <div class="float" id="picture7">pictue7</div>-->
+       <!--    <div class="float" id="picture8">picture8</div>-->
+       <!--    <div class="float" id="picture9">picture9</div>-->
+       <!--    </div>-->
+       </div>
+    </body>
+</html>
