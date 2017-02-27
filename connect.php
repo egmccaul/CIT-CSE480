186b94d4b387e37ac18a67941587db2e2c5d61d2
Emily McCauley <egmccaul@oakland.edu>
2017-02-27 13:02:19 -0500

    finally remove the database connection successful message from connect.php

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
 	try {
 		$dbh = new PDO($dsn, $user, $password);
 		/* add a message at the top of the page noting connection successful. */
-		echo 'Database connection sucessful!';
+		//echo 'Database connection sucessful!';
 	} catch (PDOException $e) {
 		/* add a message at the top of the page noting connection to database failed. */
 		echo 'Connection failed: ' . $e->getMessage();
