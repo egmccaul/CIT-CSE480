151a8804e69a348eb287158fd6680be406daeb57
Emily McCauley <egmccaul@oakland.edu>
2017-03-13 20:36:42 -0400

    Setup Client Side email message.

--- mailer.php
+++ mailer.php
@@ -22,8 +22,9 @@ if(md5($verif_box).'a4xn' == $_COOKIE['tntcon']){
 	// if verification code was correct send the message and show this page
 	$message = "Name: ".$name."\n".$message;
 	$message = "From: ".$from."\n".$message;
+	$cust_message = $message."\n\n"."We have recieved your request, and will respond momentarily.";
 	mail("egmccaul@oakland.edu", 'Online Form: '.$subject, "\n\n".$message, "From: $from \n");
-	mail("$from", 'Online Form: '.$subject, "\n\n".$message, "From: $from \n");
+	mail("$from", 'Online Form: '.$subject, "\n\n".$cust_message, "From: $from \n");
 	// delete the cookie so it cannot sent again by refreshing this page
 	header("Location:".$_SERVER['HTTP_REFERER']);
 	setcookie('tntcon','');
