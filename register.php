2e77c7890aefbe929fdb7672f75fb93196ee6530
Emily McCauley <egmccaul@oakland.edu>
2017-04-24 13:15:02 -0400

    fix registration page

--- register.php
+++ register.php
@@ -3,49 +3,65 @@
 include('header.php');
 
 // If form submitted, insert values into the database.
-	if (isset($_POST['email'])){
+	if ((isset($_POST['email'])) && (isset($_POST['fname'])) && (isset($_POST['lname'])) && (isset($_POST['password']))){
 		$fname = $_POST['fname'];
 		$lname = $_POST['lname'];
 		$email = $_POST['email'];
 		$password = $_POST['password'];
-
-		// PDO check for previous user
-			$statement = $dbh->prepare("SELECT * FROM USER WHERE USER_PASS = :pass AND USER_EMAIL = :email");
-			// PDO binds entry to protect against SQL Injection
-			$statement->bindParam(':pass', $password, PDO::PARAM_STR);
-			$statement->bindParam(':email', $email, PDO::PARAM_STR);
-
-			// Executes query
-			$executed = $statement->execute();
-
-			// Count number of rows
-			$number_rows = $statement->fetchColumn();
-
-			// If a user is found to match the criteria
-			if ($number_rows > 0) {
-				// Display a user found message.
-				?> <script>window.alert('Email Already Found!');</script> <?php
-			} else {
-
-				// PDO database entry query - (Removed state due to issues inserting into database, works after removal, defaulted to "Michigan")
-				$insertState = $dbh->prepare("INSERT INTO USER(USER_FNAME, USER_LNAME, USER_EMAIL, USER_PASS) VALUES (:fname, :lname, :email, :password)");
-				
+			
+			if (($email <> "") && ($fname <> "") && ($lname <> "") && ($password <> "")){
+	
+			// PDO check for previous user
+				$statement = $dbh->prepare("SELECT * FROM USER WHERE USER_PASS = :pass AND USER_EMAIL = :email");
 				// PDO binds entry to protect against SQL Injection
-				$insertState->bindParam(':fname', $fname, PDO::PARAM_STR);
-				$insertState->bindParam(':lname', $lname, PDO::PARAM_STR);
-				$insertState->bindParam(':email', $email, PDO::PARAM_STR);
-				$insertState->bindParam(':password', $password, PDO::PARAM_STR);
+				$statement->bindParam(':pass', $password, PDO::PARAM_STR);
+				$statement->bindParam(':email', $email, PDO::PARAM_STR);
 
 				// Executes query
-				$insertExecuted = $insertState->execute();
+				$executed = $statement->execute();
 
-				if ($insertExecuted = true){
-					// Redirects after successful to home page
-					header("Location: index.php");
-				}
+				// Count number of rows
+				$number_rows = $statement->fetchColumn();
+
+				// If a user is found to match the criteria
+				if ($number_rows > 0) {
+					// Display a user found message.
+					?> <script>window.alert('Email Already Found!');</script> <?php
+				} else {
+
+					// PDO database entry query - (Removed state due to issues inserting into database, works after removal, defaulted to "Michigan")
+					$insertState = $dbh->prepare("INSERT INTO USER(USER_FNAME, USER_LNAME, USER_EMAIL, USER_PASS) VALUES (:fname, :lname, :email, :password)");
+					
+					// PDO binds entry to protect against SQL Injection
+					$insertState->bindParam(':fname', $fname, PDO::PARAM_STR);
+					$insertState->bindParam(':lname', $lname, PDO::PARAM_STR);
+					$insertState->bindParam(':email', $email, PDO::PARAM_STR);
+					$insertState->bindParam(':password', $password, PDO::PARAM_STR);
+
+					// Executes query
+					$insertExecuted = $insertState->execute();
+
+					if ($insertExecuted = true){
+						echo "<script>window.alert('Welcome! You have successfully created an account..');</script>";
+						
+						// Redirects after successful to home page
+						header("Location: index.php");
+					} 
+				}				
+			} else {
+
+					unset($email);
+					unset($fname);
+					unset($lname);
+					unset($password);
+					
+					unset($_POST['email']);
+					unset($_POST['fname']);
+					unset($_POST['lname']);
+					unset($_POST['password']);
+					echo "<script>window.alert('Please make sure all of the fields have be filled out before submitting.');</script>";
 			}
-
-	}else{
+	}
 ?>
 
 <div class="start-screen">
@@ -96,9 +112,6 @@ include('header.php');
 </div>
 
 <?php
-		/* Closes above PHP and MySQL Query. */
-	}
-		
 /* Adds in the footer at the bottom of the page.*/
 include('footer.php');
 ?>
