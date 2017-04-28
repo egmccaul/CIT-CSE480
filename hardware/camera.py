ed0322bfdfcc9cd4290aabd5b3c1b1b559d09de1
egmccaul <egmccaul@oakland.edu>
2017-04-28 12:52:43 -0400

    Upload Hardware files
    Worked on by Tyler D and Emily on actual device.

--- hardware/camera.py
+++ hardware/camera.py
@@ -2,6 +2,7 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 from time import sleep
+import subprocess
 
 sensor = MotionSensor(14)
 camera = PiCamera()
@@ -9,5 +10,8 @@ camera = PiCamera()
 while True:
     sensor.wait_for_motion()
     filename = datetime.now().strftime("%H.%M.%S_%Y-%m-%d.jpg")
-    camera.capture(filename)
+    camera.capture("/home/pi/Pictures/" + filename)
     sleep(5)
+    cmd = "sudo bash transfer"
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
